ab4f94c20915a0594c96457fb41dc88aff307be1
amir khazama <amirkhazama1139@gmail.com>
2023-06-03 15:08:03 +0330

    included the instruction code file in the test bench

--- alu_tb.v
+++ alu_tb.v
@@ -2,14 +2,7 @@
 
 module alu_tb;
   parameter WIDTH = 8;
-  parameter HOLD = 0;
-  parameter CLEAR = HOLD + 1;
-  parameter ADD = CLEAR + 1;
-  parameter SUB = ADD + 1;
-  parameter AND = SUB + 1;
-  parameter NEG = AND + 1;
-  parameter NOT = NEG + 1;
-  parameter XOR = NOT + 1;
+  `include "alu_instruction_codes.v"
 
   reg [WIDTH-1:0] in;
   reg [2:0] control;
